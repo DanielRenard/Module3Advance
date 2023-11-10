2779d2a174a5354a87dcda7df852a7ec747f518a
DanielRenard <danieljuderenard@gmail.com>
2023-11-10 13:18:35 -0600

    added index9 and 10 files.

--- index7.js
+++ index7.js
@@ -4,24 +4,22 @@
 // However, we can define our own toString methods for custom objects to provide a more
 // meaningful string representation.
 
-
+function Person(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+}
+const person1 = new Person("James Brown", 73, "male");
+console.log("person1: " + person1); //prints person1: [object Object]
 
 // a) Define a custom toString method for the Person object that will format and print
 // their details
 
-
-
 // b) Test your method by creating 2 different people using the below constructor function
 // and printing them
-
-
 
 // c) Create a new constructor function Student that uses call to inherit from Person and
 // add an extra property cohort
 
-
-
 // d) Add a custom toString for Student objects that formats and prints their details. Test
 // with 2 students.
-
-
